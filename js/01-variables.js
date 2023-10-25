5efb7f3bb8193b76e400918a7170f7d4821ea75b
Franco <lmassa.fms@gmail.com>
2023-10-25 18:11:01 -0300

    agregamos un ejemplo de prompt y parceFloat

--- js/01-variables.js
+++ js/01-variables.js
@@ -27,4 +27,18 @@ console.log(anioActual);
 document.write('<p>Esto es una prueba de document write</p>');
 
 // ventanas emergentes
-alert('esto es una alerta');
+//alert('esto es una alerta');
+
+// solicitar a un usuario un nombre y mostrar un saludo
+const usuario = prompt('Ingrese su nombre:');
+console.log(usuario);
+
+document.write('<p>Bienvenido ' + usuario + '</p>');
+
+// solicitar a un usuario un precio de un producto en dolares
+const precioSolicitado = prompt('ingrese un valor en usd');
+//alert('el importe ingresado corresponde a $'+(precioSolicitado*1100)+ ' pesos argentinos');
+
+// parseInt('50') 
+// parseFloat(50.4)
+alert('el importe ingresado corresponde a usd$'+(parseFloat(precioSolicitado)+2));
