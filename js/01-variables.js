c737c0e7e6ba3a7738662e0bb9aaf5f42bd9a189
Franco <lmassa.fms@gmail.com>
2023-10-25 15:14:00 -0300

    declaracion de una variable y como verla en consola

---
+++ js/01-variables.js
@@ -0,0 +1,16 @@
+// Comentario de una sola linea en JS
+
+/*
+Comentario 
+de varias 
+lineas
+*/
+
+// declarar una variable (var - let - const)
+
+let anioActual = 2023;
+
+// mostrar mensajes
+console.log(anioActual);
+console.log('anioActual');
+console.log('año actual: ' +anioActual);
