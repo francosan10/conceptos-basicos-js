1fb5153c3d21684e939444e3f97be4c22479f483
Franco <lmassa.fms@gmail.com>
2023-11-08 17:19:56 -0300

    agregamos metodos para insertar elementos en el array de peliculas

--- js/05-arrays.js
+++ js/05-arrays.js
@@ -18,3 +18,36 @@ for(let indicePeliculas = 0; indicePeliculas < peliculas.length; indicePeliculas
     document.write(`<li>${peliculas[indicePeliculas]}</li>`);
 }
 document.write('<ul>');
+
+// agregar elementos al array
+// metodo unshift (agrega elementos al principio del array)
+
+peliculas.unshift('Batman', 'Ironman 1');
+
+document.write(`<h2>Lista de peliculas con 2 pelis nuevas (${peliculas.length})</h2>`);
+document.write('<ul>');
+for(let indicePeliculas = 0; indicePeliculas < peliculas.length; indicePeliculas++) {
+    document.write(`<li>${peliculas[indicePeliculas]}</li>`);
+}
+document.write('<ul>');
+
+// metodo splice (puede agregar elementos al array en la posicion que yo quiera
+peliculas.splice(3, 0, 'Scream');
+
+document.write(`<h2>Lista de peliculas, agregamos scream (${peliculas.length})</h2>`);
+document.write('<ul>');
+for(let indicePeliculas = 0; indicePeliculas < peliculas.length; indicePeliculas++) {
+    document.write(`<li>${peliculas[indicePeliculas]}</li>`);
+}
+document.write('<ul>');
+
+// metodo push para agregar un elemento al final del array
+
+peliculas.push('Gran Turismo');
+
+document.write(`<h2>Lista de peliculas, agregamos Gran Turismo (${peliculas.length})</h2>`);
+document.write('<ul>');
+for(let indicePeliculas = 0; indicePeliculas < peliculas.length; indicePeliculas++) {
+    document.write(`<li>${peliculas[indicePeliculas]}</li>`);
+}
+document.write('<ul>');
