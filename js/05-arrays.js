7ad933e4e864dd8089894f89c12bcf08dcfff886
Franco <lmassa.fms@gmail.com>
2023-11-13 17:56:46 -0300

    organizamos el archivo de peliculas con una funcion

--- js/05-arrays.js
+++ js/05-arrays.js
@@ -3,6 +3,16 @@ const personajes = [];
 
 const peliculas = ['Interestellar', 'matrix', 'lotr', 2023, true, 'Los juegos del hambre'];
 
+const mostrarPeliculas = (titulo) => {
+    document.write(`<h2>${titulo}</h2>`);
+    document.write('<ul>');
+    for(let indicePeliculas = 0; indicePeliculas < peliculas.length; indicePeliculas++) {
+        document.write(`<li>${peliculas[indicePeliculas]}</li>`);
+    }
+    document.write('<ul>');
+}
+mostrarPeliculas('Lista de Peliculas');
+
 // mostrar un array
 console.log(peliculas);
 document.write(peliculas);
@@ -20,78 +30,34 @@ for(let indicePeliculas = 0; indicePeliculas < peliculas.length; indicePeliculas
 document.write('<ul>');
 
 // agregar elementos al array
+
 // metodo unshift (agrega elementos al principio del array)
-
 peliculas.unshift('Batman', 'Ironman 1');
-
-document.write(`<h2>Lista de peliculas con 2 pelis nuevas (${peliculas.length})</h2>`);
-document.write('<ul>');
-for(let indicePeliculas = 0; indicePeliculas < peliculas.length; indicePeliculas++) {
-    document.write(`<li>${peliculas[indicePeliculas]}</li>`);
-}
-document.write('<ul>');
+mostrarPeliculas(`Lista de peliculas con 2 pelis nuevas (${peliculas.length})`);
 
 // metodo splice (puede agregar elementos al array en la posicion que yo quiera
 peliculas.splice(3, 0, 'Scream');
-
-document.write(`<h2>Lista de peliculas, agregamos scream (${peliculas.length})</h2>`);
-document.write('<ul>');
-for(let indicePeliculas = 0; indicePeliculas < peliculas.length; indicePeliculas++) {
-    document.write(`<li>${peliculas[indicePeliculas]}</li>`);
-}
-document.write('<ul>');
+mostrarPeliculas(`Lista de peliculas, agregamos scream (${peliculas.length})`);
 
 // metodo push para agregar un elemento al final del array
-
 peliculas.push('Gran Turismo');
-
-document.write(`<h2>Lista de peliculas, agregamos Gran Turismo (${peliculas.length})</h2>`);
-document.write('<ul>');
-for(let indicePeliculas = 0; indicePeliculas < peliculas.length; indicePeliculas++) {
-    document.write(`<li>${peliculas[indicePeliculas]}</li>`);
-}
-document.write('<ul>');
+mostrarPeliculas(`Lista de peliculas, agregamos Gran Turismo (${peliculas.length})`);
 
 // modificar elementos del array
 peliculas[6] = 'Inception';
-
-document.write(`<h2>Lista de peliculas, modificamos el elemento de la posicion 6 del array (${peliculas.length})</h2>`);
-document.write('<ul>');
-for(let indicePeliculas = 0; indicePeliculas < peliculas.length; indicePeliculas++) {
-    document.write(`<li>${peliculas[indicePeliculas]}</li>`);
-}
-document.write('<ul>');
+mostrarPeliculas(`Lista de peliculas, modificamos el elemento de la posicion 6 del array (${peliculas.length})`);
 
 // eliminar elementos al principio del array
 // metodo shift elimina el primer elemento del array
 peliculas.shift();
+mostrarPeliculas(`Lista de peliculas, eliminamos el primer elemento del array (${peliculas.length})`);
 
-document.write(`<h2>Lista de peliculas, eliminamos el primer elemento del array (${peliculas.length})</h2>`);
-document.write('<ul>');
-for(let indicePeliculas = 0; indicePeliculas < peliculas.length; indicePeliculas++) {
-    document.write(`<li>${peliculas[indicePeliculas]}</li>`);
-}
-document.write('<ul>');
-
-// metodo slice elimina un elemento determinado del array
-
+// metodo splice elimina un elemento determinado del array
 peliculas.splice(6, 1,);
 // peliculas.splice(6, 2,);
 // peliculas.splice(6);
-document.write(`<h2>Lista de peliculas, eliminamos un elemento del array (${peliculas.length})</h2>`);
-document.write('<ul>');
-for(let indicePeliculas = 0; indicePeliculas < peliculas.length; indicePeliculas++) {
-    document.write(`<li>${peliculas[indicePeliculas]}</li>`);
-}
-document.write('<ul>');
+mostrarPeliculas(`Lista de peliculas, eliminamos un elemento del array (${peliculas.length})`);
 
 // metodo pop elimina el ultimo elemento del array
-
 peliculas.pop();
-
-document.write(`<h2>Lista de peliculas, eliminamos el último elemento del array (${peliculas.length})</h2>`);
-document.write('<ul>');
-for(let indicePeliculas = 0; indicePeliculas < peliculas.length; indicePeliculas++) {
-    document.write(`<li>${peliculas[indicePeliculas]}</li>`);
-}
-document.write('<ul>');
+mostrarPeliculas(`Lista de peliculas, eliminamos el último elemento del array (${peliculas.length})`);
