d67c46d9c91bc11ff63253d23060a79184fc5b6d
Franco <lmassa.fms@gmail.com>
2023-11-23 11:44:00 -0300

    agregamos un ejemplo de herencia y polimorfismo con la clase pokemon legendario

--- js/08-clases.js
+++ js/08-clases.js
@@ -37,6 +37,53 @@ class Pokemon {
         </ul>`)
     }
 };
+//Aplicar HERENCIA
+class PokemonLegendario extends Pokemon {
+    #rareza;
+    #ataqueEspecial;
+
+    constructor(nombre, tipo, peso, altura, evolucion, ataques, numPokedex, fuerza, rareza, ataqueEspecial) {
+        super(nombre, tipo, peso, altura, evolucion, ataques, numPokedex, fuerza)
+        this.#rareza = rareza;
+        this.#ataqueEspecial = ataqueEspecial;
+    }
+
+    get rareza () {
+        return this.#rareza;
+    }
+    set rareza (nuevaRareza) {
+        if (nuevaRareza.length >= 3 && nuevaRareza.length <= 10) {
+            this.#rareza = nuevaRareza;
+        } else {
+            alert('Ingresaste un valor erroneo');
+        }
+    }
+    get ataqueEspecial () {
+        return this.#ataqueEspecial;
+    }
+    set ataqueEspecial (nuevoAtaqueEspecial) {
+        this.#ataqueEspecial = nuevoAtaqueEspecial;
+    }
+    // agregar mas metodos
+    mostrarDatosNuevos () {
+
+    }
+    // polimorfismo
+    mostrarDatos () {
+        document.write(`<h2>Pokemon: ${this.nombre}</h2>
+        <ul>
+        <li>Num Pokedex: ${this.numPokedex}</li>
+        <li>Tipo: ${this.tipo}</li>
+        <li>Peso: ${this.peso}</li>
+        <li>Altura: ${this.altura}</li>
+        <li>Evolucion: ${this.evolucion}</li>
+        <li>Rareza: ${this.rareza}</li>
+        <li>Ataques especiales: ${this.ataqueEspecial}</li>
+        </ul>`)
+    }
+}
+
+
 // crear o instanciar un objeto
 const charmander = new Pokemon ('Charmander', 'fuego', 15, 0.6, ['charmeleon', 'charizard'], ['blaze', 'ascuas', 'lanzallamas'], 4, 4);
 const togepi = new Pokemon ('Togepi', 'hada', 1.5, 0.3, ['Togetic', 'Togekiss'], ['gracia serena', 'dicha y entusiasmo', 'placaje'], 175, 3 )
@@ -53,5 +100,12 @@ document.write(`<p>Tipo: ${charmander.tipo}</p>`);
 charmander.mostrarDatos();
 togepi.mostrarDatos();
 
+const mewtwo = new PokemonLegendario('Mewtwo','psiquico', 122, 2, [], ['placaje','Confusión'],150, 7, 'Infrecuente',['charge beam', 'electro ball']);
 
+mewtwo.mostrarDatos();
 
+// 4 pilares de la programacion
+// abstraccion
+// encapsulamiento
+// herencia
+// polimorfismo
